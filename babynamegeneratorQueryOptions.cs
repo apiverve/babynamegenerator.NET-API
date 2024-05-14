87abcdad2fa8781291cf0c174c8d9a23928e17f5
APIVerve Team <hello@apiverve.com>
2024-05-14 15:20:52 -0500

    Bug fixes and Improvements

---
+++ babynamegeneratorQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class babynamegeneratorQueryOptions {
+public string gender { get; set; }
+public int count { get; set; }
+}
+
+}
